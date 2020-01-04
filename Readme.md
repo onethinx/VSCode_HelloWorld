4b2b47a20bc5cd44f58c9d2228ac078e98b33841
Rolf <rolf@rolf-mac-pro-2.home>
2020-01-04 21:23:57 +0100

    Initial project setup

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Hello World!
+
+This is the Onethinx hello world program.
+
+# Preparation
+
+1. Make sure you have the [VS Code IDE setup](https://github.com/onethinx/VSCodeHelloWorld/blob/master/Readme_SetupVScode.md) done.
+1. Clone or download the project to your local machine.
+1. Start VS Code
+1. Load the project: File >> Open >> Select the folder of the project >> OK
+1. Navigate to source/OnethinxCore/LoRaWAN_keys.h and fill in your LoRaWAN keys.
+1. Click "⚙︎ Build" in the status bar to build the project.
+    * if it fails to build, try "Clean Reconfigure" + "Clean Rebuild" from the Cmake extension (left bar).
+    * if it still fails, try deleting the contents of the 'build/' folder.
+1. Debug the project using the (Cortex-) Debug extension from the left bar.
+    * depending on your debug adapter select "CM4 Debug Kitprog" or "CM4 Debug J-Link" and press 'start'.
+
+# Description
+
+The blue LED will first be on during the join procedure.
+If successfully joined, the blue LED will turn off for one second. 
+A loop where the char-values of 'Hello World' will be sent every 10s is then entered.
+If not joined -this may take a long while-, the blue LED will blink fast forever.
+
+# IDE Setup (VS Code for use with the Onethinx Core)
+
+If you not yet have set-up VS Code, please follow [this guide](https://github.com/onethinx/VSCodeHelloWorld/blob/master/Readme_SetupVScode.md).
---
+++ source/main.c
@@ -0,0 +1,122 @@
+/* ==========================================================
+ *    ___             _   _     _
+ *   / _ \ _ __   ___| |_| |__ (_)_ __ __  __
+ *  | | | | '_ \ / _ \ __| '_ \| | '_ \\ \/ /
+ *  | |_| | | | |  __/ |_| | | | | | | |>  <
+ *   \___/|_| |_|\___|\__|_| |_|_|_| |_/_/\_\
+ *
+ * Copyright Onethinx, 2018
+ * All Rights Reserved
+ *
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF Onethinx BV
+ *
+ * ==========================================================
+*/
+#include "OnethinxCore01.h"
+#include "DemoKit01.h"
+#include "LoRaWAN_keys.h"
+#include "cycfg.h"
+
+/* Go to ../OnethinxCore/LoRaWAN_keys.h and fill in the fields of the TTN_OTAAkeys structure */
+
+coreConfiguration_t	coreConfig = {
+	.Join.KeysPtr = 		&TTN_OTAAkeys,
+	.Join.DataRate =		DR_AUTO,
+	.Join.Power =			PWR_MAX,
+	.Join.MAXTries = 		100,
+    .Join.SubBand_1st =     EU_SUB_BANDS_DEFAULT,
+	.Join.SubBand_2nd =     EU_SUB_BANDS_DEFAULT,
+	.TX.Confirmed = 		false,
+	.TX.DataRate = 			DR_0,
+	.TX.Power = 			PWR_MAX,
+	.TX.FPort = 			1,
+};
+
+/*******************************************************************************arm-none-eabi-gcc
+* Function Name: main()
+********************************************************************************
+* Summary:
+*  Sends a "Hello World" message using LoRaWAN.
+* Details:
+*  Go to ../OnethinxCore/LoRaWAN_keys.h and fill in the fields of the TTN_OTAAkeys structure
+*  Read the quickstart guide on how to create keys.
+*******************************************************************************/
+
+/* OnethinxCore uses the following structures and variables, which can be defined globally */
+coreStatus_t 	coreStatus;
+coreInfo_t 		coreInfo;
+
+uint8_t RXbuffer[64];
+uint8_t TXbuffer[64];
+
+int main(void)
+{
+	uint8_t j=0;
+
+	/* initialize hardware configuration */
+	init_cycfg_all();
+
+	/* enable global interrupts */
+	__enable_irq();
+
+	/* Blue LED ON */
+	LED_B_SET(LED_ON);
+
+	/* initialize radio with parameters in coreConfig */
+	coreStatus = LoRaWAN_Init(&coreConfig);
+
+	/* Check Onethinx Core info */
+	LoRaWAN_GetInfo(&coreInfo);
+
+	/* send join using parameters in coreConfig, blocks until either success or MAXtries */
+	coreStatus = LoRaWAN_Join(true);
+
+	/* check for successful join */
+	if (!coreStatus.mac.isJoined){
+		while(1) {
+			LED_B_INV;
+			CyDelay(100);
+		}
+	} else {
+		LED_B_SET(LED_OFF);
+		/*delay before first message will be sent */
+		CyDelay(1000);
+	}
+
+	/* main loop */
+	for(;;)
+	{
+		LED_B_SET(LED_ON);
+
+		/* compose a message to send */
+        j=0;
+        TXbuffer[j++] = 0x48; /* H */
+		TXbuffer[j++] = 0x45; /* E */
+		TXbuffer[j++] = 0x4c; /* L */
+		TXbuffer[j++] = 0x4c; /* L */
+		TXbuffer[j++] = 0x4f; /* O */
+		TXbuffer[j++] = 0x20; /*   */
+		TXbuffer[j++] = 0x57; /* W */
+		TXbuffer[j++] = 0x4f; /* O */
+		TXbuffer[j++] = 0x52; /* R */
+		TXbuffer[j++] = 0x4c; /* L */
+		TXbuffer[j++] = 0x44; /* D */
+        coreStatus = LoRaWAN_Send((uint8_t *) TXbuffer, j, true);
+		CyDelay(1000);
+        if( coreStatus.system.errorStatus == system_BusyError ){
+        	for(int i=0; i<10; i++){
+				LED_B_INV;
+				CyDelay(100);
+        	}
+        }
+		LED_B_SET(LED_OFF);
+
+		/* wait before sending next message */
+		CyDelay( 10000 );
+	}
+}
+
+
+/* [] END OF FILE */
